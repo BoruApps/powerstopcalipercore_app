89e3c49351e665afd9ef8c11fa81475e6fafc1c5
chukwumaokere <chukwumaokere@yahoo.com>
2021-03-24 18:47:17 -0500

    update

--- src/app/folder/customer-returns/customer-returns.page.ts
+++ src/app/folder/customer-returns/customer-returns.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-customer-returns',
   templateUrl: './customer-returns.page.html',
@@ -10,6 +10,7 @@ export class CustomerReturnsPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    Swal.fire('Testing', 'Test', 'success');
   }
 
 }
